6fb8bd32a4c6f68ec2fc6bee9f1bbbc0c00acfa4
ANDRII STO <100497629+AndriiStol@users.noreply.github.com>
2024-09-24 17:34:03 +0300

    Update AnimationPlayer.cs

--- AnimationPlayer.cs
+++ AnimationPlayer.cs
@@ -10,22 +10,22 @@ public class AnimationPlayer : MonoBehaviour
     {
         animator = GetComponent<Animator>();
 
-        // Воспроизводим анимацию с именем "tap" при запуске сцены.
+        
         if (animator != null)
         {
             animator.Play("tap");
         }
 
-        // Запускаем корутину для скрытия объекта после завершения анимации.
+        
         StartCoroutine(HideAfterAnimation());
     }
 
     IEnumerator HideAfterAnimation()
     {
-        // Ждем завершения анимации.
+        
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
 
-        // Скрываем объект.
+        
         gameObject.SetActive(false);
     }
 }
