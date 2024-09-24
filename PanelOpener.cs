d604706d21788ec2219f094ca3b5de82fe199604
ANDRII STO <100497629+AndriiStol@users.noreply.github.com>
2024-09-24 17:36:00 +0300

    Update PanelOpener.cs

--- PanelOpener.cs
+++ PanelOpener.cs
@@ -5,32 +5,32 @@ using UnityEngine.UI;
 
 public class PanelOpener : MonoBehaviour
 {
-    public Button pauseButton; // Ссылка на кнопку паузы.
-    public GameObject panel; // Ссылка на вашу панель.
+    public Button pauseButton; 
+    public GameObject panel; 
     public GameObject panel1;
-    private bool isPanelOpen = false; // Флаг, который определяет, открыта ли панель.
+    private bool isPanelOpen = false; 
     private bool isPanel1Open = false;
 
 
 
-    // Метод, вызываемый при нажатии на кнопку.
+    
     public void TogglePanel()
     {
-        // Проверяем состояние панели и меняем его.
+       
         isPanelOpen = !isPanelOpen;
 
-        // Включаем или выключаем панель в зависимости от состояния.
+       
         panel.SetActive(isPanelOpen);
     }
 
 
     public void TogglePanel1()
     {
-        // Проверяем состояние панели и меняем его.
+        
         isPanel1Open = !isPanel1Open;
 
 
-        // Включаем или выключаем панель в зависимости от состояния.
+        
         panel1.SetActive(isPanel1Open);
 
         pauseButton.gameObject.SetActive(!isPanel1Open);
